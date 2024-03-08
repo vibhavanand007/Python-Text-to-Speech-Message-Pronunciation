c047668ea273052ae8392462ee4c35c0fb557b2b
Vibhav Anand <100696082+vibhavanand007@users.noreply.github.com>
2024-03-08 12:43:00 +0530

    Add files via upload
    With this program, users can input text messages, and the program will utilize text-to-speech (TTS) technology to convert the text into audible speech, which is then pronounced through the computer's speakers.
    win32com.client Module: Utilized to interact with the Windows Text-to-Speech (TTS) service for speech synthesis.

---
+++ Python Text-to-Speech Message Pronunciation.py
@@ -0,0 +1,17 @@
+#Speaker
+import win32com.client as wincl
+import time
+
+print(">>What you want me to speak\n>type 'quit()' to exit")
+while True:
+    sentence = input("")
+    time.sleep(2)
+    if (sentence == "quit()"):
+        speaker = wincl.Dispatch("SAPI.SpVoice")
+        message = "Thank you for using this program"
+        speaker.Speak(message)
+        break
+    else:
+        speaker = wincl.Dispatch("SAPI.SpVoice")
+        message = f"{sentence}"
+        speaker.Speak(message)
